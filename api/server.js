38f71b3570bd08a09cdd0e10ebc25f1e1c1514e7
kp1129 <katya.p.1129@gmail.com>
2020-04-26 23:36:09 -0700

    seed users data

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const authRouter = require('../auth/authRouter');
+const authenticator = require('../auth/authenticator');
 
 const server = express();
 
@@ -8,6 +9,12 @@ server.use(helmet());
 server.use(express.json());
 
 server.use("/api", authRouter);
+
+// delete the code below after we get some more routers going
+// this was just a middleware test
+server.get("/api/test", authenticator, (req, res) => {
+    res.status(200).json({ message: "if you can see this, the authenticator works" })
+})
 
 
 
