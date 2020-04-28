8ac84d6bd1960d1459b480724b2ea4de86463ad5
kp1129 <katya.p.1129@gmail.com>
2020-04-27 20:07:54 -0700

    add more tests; authentication endpoints thoroughly tested with both postman and unit tests

--- auth/authRouter.test.js
+++ auth/authRouter.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const server = require('../api/server');
 const db = require('../database/connection');
 
-describe('authentication', () => {
+describe('authentication endpoints', () => {
     describe('POST /signup', () => {
         beforeEach(async () => {
             await db('users').truncate();
@@ -32,5 +32,56 @@ describe('authentication', () => {
                         expect(res.body.message).toBe("sign up successful");
                     })
         })
-    });    
+    });
+    
+    describe('POST /login', () => {
+        beforeEach(async () => {
+            await db('users').truncate();
+        });
+        it('should return message "login successful', async () => {
+            // first register a user
+            await request(server)
+                    .post("/api/signup")
+                    .send({
+                        username: "brandnewuser",
+                        password: "brandnewpassword",
+                        phoneNumber: "3213211234"
+                    })
+                    .then(res => {
+                        expect(res.status).toBe(201);
+                    })
+            await request(server)
+                    .post("/api/login")
+                    .send({
+                        username: "brandnewuser",
+                        password: "brandnewpassword"
+                    })
+                    .then(res => {
+                        expect(res.body.message).toBe("login successful")
+                    })        
+        });
+
+        it('should return status code 200', async () => {
+            // first register a user
+            await request(server)
+                    .post("/api/signup")
+                    .send({
+                        username: "helloitsme",
+                        password: "letmein",
+                        phoneNumber: "3213211235"
+                    })
+                    .then(res => {
+                        expect(res.status).toBe(201);
+                    })
+            await request(server)
+                    .post("/api/login")
+                    .send({
+                        username: "helloitsme",
+                        password: "letmein"
+                    })
+                    .then(res => {
+                        expect(res.status).toBe(200);
+                    })        
+        })      
+    })
 })
